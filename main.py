4571bc67bb2e4434b6c42722d22f8a62d50e3bf4
studioseeds <terui@studio-seed-s.com>
2023-08-14 18:26:20 +0900

    [Update] 画像ファイルをアップロードして、ダミーの結果を表示出来るようにした

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pandas as pd
 from PIL import Image
 import time
 
-st.title('Stream 超入門')
-
 # st.write('DataFrame')
 
 # df = pd.DataFrame({
@@ -89,17 +87,56 @@ st.title('Stream 超入門')
 # expander.write('問い合わせを書く')
 
 #プログレスバー
-st.write('プログレスバーの表示')
-st.write('Start!!')
+# st.write('プログレスバーの表示')
+# st.write('Start!!')
 
-latest_iteration = st.empty()
-bar = st.progress(0)
+# latest_iteration = st.empty()
+# bar = st.progress(0)
 
-for i in range(100):
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+# for i in range(100):
+#   latest_iteration.text(f'Iteration {i+1}')
+#   bar.progress(i + 1)
+#   time.sleep(0.1)
 
-st.write('Done!!')
+# st.write('Done!!')
 
+def measure_object_length(image_path):
+    """
+    この関数は、提供された画像から物体の長さを計測することを目的としています。
+    現在は、指定された結果の画像とダミーの計測データをそのまま返しています。
+    """
+    # ダミーの結果画像
+    result_img_path = 'result.png'
+    
+    # 固定番号を持たないダミーの計測データ
+    measurements = [259.0284, 260.1915, 244.7583]
+    
+    return result_img_path, measurements
 
+# タイトルを設定
+st.title('物体の長さ計測アプリ')
+
+# 画像をアップロード
+uploaded_file = st.file_uploader("画像ファイルを選択してください (jpg, png)", type=['jpg', 'png'])
+
+if uploaded_file is not None:
+    # アップロードされた画像を表示
+    st.image(uploaded_file, caption='アップロードされた画像', use_column_width=True)
+    
+    # 計測ボタン
+    if st.button('計測'):
+        # 関数を使用
+        result_image_path, measurements = measure_object_length(uploaded_file)
+
+        # 計測結果の画像を表示
+        result_image = Image.open(result_image_path)
+        st.image(result_image, caption='計測結果', use_column_width=True)
+
+        # 番号を動的に採番して計測データのテーブルを作成
+        data = {
+            'No.': list(range(1, len(measurements) + 1)),  # ここで 'No.' カラムを明示的に作成
+            '長さ': measurements
+        }
+        df = pd.DataFrame(data).set_index('No.')
+
+        st.write(df)  # DataFrameを表示
